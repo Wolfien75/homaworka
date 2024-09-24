59e98d1fc84f836571be3c2d9e6c1b46bd099fbf
Volk-off <whatits.lolol@internet.ru>
2024-09-24 21:27:23 +0300

    main things added

--- ConsoleApplication81/ConsoleApplication81.cpp
+++ ConsoleApplication81/ConsoleApplication81.cpp
@@ -3,24 +3,46 @@
 using std::cout, std::cin,std::endl;
 
 int main() {
+
     short menuCh = 0;
-    int n1 = 4;
-    int n2 = 2;
+    int n1 = 0;
+    int n2 = 0;
+    char oper = ' ';
 
     cout << "Hi! This is calculator!\n"
         << "Choose the required operation: \n"
-        << "1 - calculate, 2 - exit\n";
+        << "1 - calculate, 2 - exit" << endl;
     cin >> menuCh;
 
-    switch(menuCh){
+    switch (menuCh) {
     case 1:
-        cout << n1 << "+" << n2 << endl;
-        cout << n1 << "-" << n2 << endl;
-        cout << n1 << "*" << n2 << endl;
-        cout << n1 << "/" << n2 << endl;
-        break;
-    case 2:
-        break;
+        cout << "Enter first number" << endl;
+        cin >> n1;
+        cout << "Enter second number" << endl;
+        cin >> n2;
+        cout << "Choose operation" << endl;
+        cin >> oper;
+        cout << endl;
+        switch (oper) {
+        case '+':
+            cout << n1 << "+" << n2 << "=" << n1 + n2 << endl;
+            break;
+
+        case '-':
+            cout << n1 << "-" << n2 << "=" << n1 - n2 << endl;
+            break;
+
+        case '*':
+            cout << n1 << "*" << n2 << "=" << n1 * n2 << endl;
+            break;
+
+        case '/':
+            cout << n1 << "/" << n2 << "=" << n1 / n2 << endl;
+            break;
+
+        case 2:
+            break;
+        }
     }
     return 0;
 }
